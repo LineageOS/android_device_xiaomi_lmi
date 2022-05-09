921fed1f3df62cfcb5235128ce2b7b556eaa2d8c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-09 17:01:22 +0200

    lmi: Go away Megvii
    * This commit nukes Megvii proprietary files and patches camera HAL to not die when we provide our library shims
    * Fun fact: this may be intentional, our camera HAL causes a SIGSEGV while trying to call `(*_pclose)(4)`, so we just replace that with a NOP opcode
    
    Change-Id: I53f8f0897b25517741a06c5962c02fe8fda61072

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,9 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/hw/camera.qcom.so)
+            "${SIGSCAN}" -p "CB 66 40 F9 60 01 3F D6" -P "CB 66 40 F9 1F 20 03 D5" -f "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
